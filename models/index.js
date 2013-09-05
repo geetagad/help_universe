c4c47364965174cf90947f213da9ecc780a89033
geetagad <geetagad@gmail.com>
2013-09-05 00:25:01 +0000

    Changed path  .pgpass

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ if (!global.hasOwnProperty('db')) {
     var Sequelize = require('sequelize');
     var sq = null;
     var fs = require('fs');
-    var PGPASS_FILE = '/.pgpass';
+    var PGPASS_FILE = '.pgpass';
     //var PGPASS_FILE = '../.pgpass';
     if (process.env.DATABASE_URL) {
         /* Remote database
